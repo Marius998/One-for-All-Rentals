dda1735df8c6ead1aece97169ae8697dc135857c
Felix Guenthner <felix-guenthner@web.de>
2019-08-26 14:14:18 +0200

    add google maps api

---
+++ rental-webapp/src/components/gmap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <GmapMap
+      class="gmap"
+      :center="{lat:50.946256, lng:6.897077}"
+      :zoom="16"
+      map-type-id="terrain"
+      :options="{
+   zoomControl: false,
+   mapTypeControl: false,
+   scaleControl: false,
+   streetViewControl: false,
+   rotateControl: false,
+   fullscreenControl: false,
+   disableDefaultUi: false
+ }"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      />
+    </GmapMap>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+
+<style scoped>
+.gmap {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+
+
--- rental-webapp/src/main.js
+++ rental-webapp/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,10 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDP0J6PujCjhbuKcqJOfXvuiwgxyGYXKOc'
+    
+  },
+})
