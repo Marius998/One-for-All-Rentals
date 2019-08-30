1f58286552df719c70a6ed417d9e22b8de4e8c52
Felix Guenthner <felix-guenthner@web.de>
2019-08-30 09:44:07 +0200

    add appBar component

---
+++ rental-webapp/src/components/appBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span @click="panToCurrent">Rental App</span>
+      </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+    
+  },
+  methods : {
+      panToCurrent () {
+          console.log("klick");
+          this.$refs.gmap.panToCurrent();
+      }
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
