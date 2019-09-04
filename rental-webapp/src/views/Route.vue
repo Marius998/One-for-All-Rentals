cca45fabb82111740009d4366867bf8a462fbc81
Felix Guenthner <felix-guenthner@web.de>
2019-09-04 17:42:41 +0200

    route component  + route view erstellt

---
+++ rental-webapp/src/views/Route.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <route/>
+  </div> 
+</template>
+
+
+<script>
+import route from "../components/route";
+
+export default {
+  name: "App",
+  components: {
+    route,
+    
+  },
+  data: () => ({
+    
+  }),
+};
+</script>
