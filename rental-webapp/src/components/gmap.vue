f58daede6c9ab7f18cf841cfdd9a9c9cf803898e
Carlos <carlosbystron@yahoo.de>
2019-09-07 18:57:46 +0200

    Überarbeitung des Direction API Zugriffs

--- rental-webapp/src/components/gmap.vue
+++ rental-webapp/src/components/gmap.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <app-route :userPos="userPosition" :choosenProvider="choosenProvider"></app-route>
+
     <v-row justify="center">
       <radial-menu
         class="radial-menu"
@@ -11,9 +13,9 @@
         <radial-menu-item>
           <!-- Ringo -->
           <img
-            @click="showRhingo=!showRhingo"
+            @click="choosenProvider.showRhingo=!choosenProvider.showRhingo"
             class="radialImg"
-            v-bind:class="{ activeModul: showRhingo}"
+            v-bind:class="{ activeModul: choosenProvider.showRhingo}"
             src="https://lh3.googleusercontent.com/g8npIP3QyCPMq4SN3cTXhnlRAg6F75qVFphxrosqmca372eTjFmPavfRCJDF6aMu9SdweC4B4EnK-CzCTYKicy-6Zfpq7ABognRKmC5UBqAOJdOOPzdN3h4iN_gfIB7Yiwm76DUWheMreKsjKOn7a8dWRf778HERlZ6BcDH4Gd2F2E3XJlB9ks2-X9KaB_-i7f77mFITWstUnydCx0awWLkphsSkmOi0IH9-4wwX4UVfTXBr5zMxBHm4vtKppBL6Rk6AeZ7OtJ_GNB4YWxpA2ePBaZP-hzCAm26BUxdDBBt5FAJ0Jf55QuRA8LQQPEgYXWA9_t19U6JarWcdOkywICbdEnLApYmK89GlVDwH6TpaRoGQhvbKow9_01lYQyEIkopkaLLBKA8kkSPdsStuMuYSIc3sPsPumcVIMHSGEX7ALZl1p5_CCZv3yFF6gW-BdXssg-WofEwHWla0aTRynoOmE9a58TpmfJMA19VISun5tFKVvd_C7QwRm6EHGt89g7vRUo8OSEJzBkJw7pvM5yVR8_gx_wueRchfDkCTnl_T2RGk4eAFuA2pBSjOrdlithjBSfWjkeYgY4UopccXI5Fmax-943a61F2FlNI_kYosEWzaoDg8Lkb_bsL9sOiKMAKxiTooV9QezFN4O9APxmocNfaQF5Gb19Xt502uZ9hV1NutvVJUPQ=s100-no"
             alt
             srcset
@@ -23,9 +25,9 @@
         <radial-menu-item>
           <!-- Tier -->
           <img
-            @click="showTier=!showTier"
+            @click="choosenProvider.showTier=!choosenProvider.showTier"
             class="radialImg"
-            v-bind:class="{ activeModul: showTier}"
+            v-bind:class="{ activeModul: choosenProvider.showTier}"
             src="https://lh3.googleusercontent.com/FRm5mDsAk822-3Zmi-NmG_shq0jyiaqgh81gNEjsoy7nrC6ZmevioEtWSqN4Ca8Vqnpp8XhOcJmp9mp7Z66AUpLP6lWAr-4m2Si8hQHmmCzTcfGl_be61n5iWdzNjdHjGbgjryrgnyOYwK3IRLhlOmE4wdAKq0bu4gvDMuzcw_QHiZwCEFWHyALS07tUTQQ6oQUzXy3bAFPwjoGPscoDZKqD7ej7KViMJqBy0CqdlkQKZp-5KJtBR_Y8KOXO8I4brj3jbfIBp2PHjnUsdvEeVMMkOpJaP8576Ceahiatr6-jIlvRfRNfrKUuav3WlzlPkMmhk0GcWQFkul0AoatC48sGC6idSmFvtpVnUMUPnj_8xlSQWD3ZVXnGysbjOviqZP3edNc30df7F4guU-mZYEeJW4HTJ7G47S9bASbUN9Qii23ZOEYlvgizrBn7XLuxImX0ozV5hbea7pABAehxdcArixaUqfp0hYQr5D5PI3WOJmzDVuJIc6HJ1EfmqZ4T-0q5W8omQMYgoADlSvp9xMuyA2mAWb1S8V8jAYsay0blPQTNDZ9E_-F9WO6ad0DC7aK7caG5Ue_ilrYpM9n1tAKO6knxarXK07sy8fvn4kfShQrLllEAOObudFJe_rVUBY9LGHDQlD9FoU8a9c_Iuvk9JkZRXMmBxGf7Ae51Ejj6CNYVFEvEeg=s492-no"
             alt
           />
@@ -34,9 +36,9 @@
         <radial-menu-item>
           <!-- Nextbike -->
           <img
-            @click="showNextBikes=!showNextBikes"
+            @click="choosenProvider.showNextBikes=!choosenProvider.showNextBikes"
             class="radialImg"
-            v-bind:class="{ activeModul: showNextBikes}"
+            v-bind:class="{ activeModul: choosenProvider.showNextBikes}"
             src="https://lh3.googleusercontent.com/YmXUsCJPHTC_SBZLVfnlVXNvoq4I-2_x6FN5hPa-l1et1cKW82wzGU7K6L6sAxWrt2J4X97B7Cvk7OKVxSv_-ahAygLqTeqgQqS7rFpAAi50Ab1JFkBcxBoV_Od29Dj9aBhlQQdXuenMYTpvKkbcorZAQeDhbAvU7B5q_21iKfzzaNgenhb-1i9dn3W56EG-r5anawdg4pyzJPHXFmOMZIb0zlxMhSfGxWE2-Ws0VwvNI1so2FJPhgfyQxMFWOeY_zEUc6KrEQkCkxNq-3qDtDNT7YrFaGQH5xCX8t22Z3aUpQWyesUyXJDIH_nbVvX49NGbmJ5OrJAEcDSu5YJb6zanQBdoQUquLNehgmRs7QJnkQAScy_f1mA1VPKSZjoU8YqJRVT6QQ6ynnIwOC8QWRQ1zchGrAvbI1FVpNfnCtaZ8T2Qh9nJoTiO4UAxAhwYAJRTBDJ8fvNPP2MOJJxE_OejHlBVIN0w3kwlpmKrOUFgcvPODUbwLSdx95zuwb9fK9nC60LlMs4Yu-FhQkdNmmi9P816QtM_FI7ZdlAOdCcgrGJzyX1cebXw1icebCL0YpB1gnqO2mqByLLEgUIJ2HCk7K3i2Bo6fE22-nny7byJR3mpBttpWbB7je5VqKxEtRNPDN8b31oPTjALkxMeOXZE6u5YfBM0hWNnZ2aCyTt8cHEXPup2wQ=s100-no"
             alt
           />
@@ -179,6 +181,7 @@ const fetch = require("node-fetch");
 
 import { RadialMenu, RadialMenuItem } from "vue-radial-menu";
 import InfoCard from "./infoCard";
+import route from "./route";
 
 
 import * as fetchNextbike from "@/scripts/nextBike";
@@ -189,6 +192,7 @@ import { constants } from "crypto";
 export default {
   name: "gmap",
   components: {
+    'app-route':route,
     InfoCard,
     RadialMenu,
     RadialMenuItem,
@@ -197,15 +201,21 @@ export default {
 
   data() {
     return {
-      nextBikes: [], // speichert die next Bikes
-      showNextBikes: true, // entscheidet ob next Bikes als Marker dargestellt werden sollen
-      rhingo: [], // speichert die next Rhingo Vehicle
-      showRhingo: true, // entscheidet ob Rhingo Vehicle als Marker dargestellt werden sollen
-      tier: [], // speichert die next Tier Vehicle
-      showTier: true, // entscheidet ob Tier Vehicle als Marker dargestellt werden sollen
+      // speichert welche Anbieter ausgewählt wurden und als Marker dargestellt werden
+      choosenProvider: {
+        showNextBikes: true,
+        showRhingo: true,
+        showTier: true
+      },
+      nextBikes: [], // speichert die nextBikes
+      rhingo: [], // speichert die Rhingo Vehicle
+      tier: [], // speichert die Tier Vehicle
+
       display: false, // entscheidet um die infoCard angezeigt werden soll
       currentScooter: Object, // speichert das ausgewählte Vehicle für die infoCard
-      currentScooters: [], // speichert die gewünschten Vehicle, welche als GmapMarker angezeigt werden | z.B nur Rhino oder im Radius 500m
+
+      // speichert die gewünschten Vehicle, welche als GmapMarker angezeigt werden | z.B nur Rhino oder im Radius 500m
+      currentScooters: [], 
       userPosition: {
         lat: 0,
         lng: 0
@@ -328,22 +338,22 @@ export default {
 
   watch: {
     nextBikes: function() {
-      if (this.showNextBikes) {
+      if (this.choosenProvider.showNextBikes) {
         this.addMarker(this.nextBikes);
       }
     },
     rhingo: function() {
-      if (this.showRhingo) {
+      if (this.choosenProvider.showRhingo) {
         this.addMarker(this.rhingo);
       }
     },
     tier: function() {
-      if (this.showTier) {
+      if (this.choosenProvider.showTier) {
         this.addMarker(this.tier);
       }
     },
     showNextBikes: function() {
-      if (this.showNextBikes) {
+      if (this.choosenProvider.showNextBikes) {
         this.addMarker(this.nextBikes);
       } else {
         this.removeMarker("Nextbike");
@@ -351,7 +361,7 @@ export default {
     },
 
     showRhingo: function() {
-      if (this.showRhingo) {
+      if (this.choosenProvider.showRhingo) {
         this.addMarker(this.rhingo);
       } else {
         this.removeMarker("Rhingo");
@@ -359,7 +369,7 @@ export default {
     },
 
     showTier: function() {
-      if (this.showTier) {
+      if (this.choosenProvider.showTier) {
         this.addMarker(this.tier);
       } else {
         this.removeMarker("Tier");
