630e200a3f8848d2ec2ba551d97d21700ecdf64f
Carlos <carlosbystron@yahoo.de>
2019-09-21 10:03:22 +0200

    API Zugriff korigiert

--- rental-webapp/src/scripts/fordbike.js
+++ rental-webapp/src/scripts/fordbike.js
@@ -4,18 +4,18 @@ export function fetchFordbike() {
 
     return new Promise((resolve, reject) => {
 
+        console.log("fetchingFordBike");
+
         var header = {
             method: 'GET',
             headers: {
-                'Accept': 'application/json',
                 'Authorization': 'Bearer 988f911d787a1258c831869edaff6f57',
             }
         }
 
-        dbBike = "https://api.deutschebahn.com/flinkster-api-ng/v1/areas?lat=50.941278&lon=6.958281&providernetwork=2"
+        let dbBike = "https://api.deutschebahn.com/flinkster-api-ng/v1/areas?lat=50.941278&lon=6.958281&providernetwork=2"
 
         var list = [];
-        console.log("fetchingFordBike");
 
         fetch(dbBike, header)
             .then(res => res.json())
@@ -24,22 +24,22 @@ export function fetchFordbike() {
                 var gebiet1 = data.items[1].geometry.position.coordinates[0]
                 var gebiet2 = data.items[2].geometry.position.coordinates[0]
 
-                gebiet1.forEach(bike => {
+                gebiet2.forEach(bike => {
 
-                    bikeList.push({
+                    list.push({
                         'provider': 'Fordbike',
                         'id': false,
                         'battery': false,
                         'batteryLevel': null,
-                        'icon': "https://img.icons8.com/dusk/64/000000/vagina.png",
-                        'logo': "https://img.icons8.com/dusk/64/000000/vagina.png",
-                        'vehicleImg': "https://img.icons8.com/dusk/64/000000/vagina.png",
-                        'lat': bike[0],
-                        'lng': bike[1],
-                        'color' : '#3D8999', 
-                        'pricePerUnit' : "1 € / 30 Min.",
-                        'website' : "https://www.fordpass-bike.de/de",
-                        'deeplink' : 'nextbike://'
+                        'icon': "https://img.icons8.com/color/50/000000/bicycle.png",
+                        'logo': "https://lh3.googleusercontent.com/YmXUsCJPHTC_SBZLVfnlVXNvoq4I-2_x6FN5hPa-l1et1cKW82wzGU7K6L6sAxWrt2J4X97B7Cvk7OKVxSv_-ahAygLqTeqgQqS7rFpAAi50Ab1JFkBcxBoV_Od29Dj9aBhlQQdXuenMYTpvKkbcorZAQeDhbAvU7B5q_21iKfzzaNgenhb-1i9dn3W56EG-r5anawdg4pyzJPHXFmOMZIb0zlxMhSfGxWE2-Ws0VwvNI1so2FJPhgfyQxMFWOeY_zEUc6KrEQkCkxNq-3qDtDNT7YrFaGQH5xCX8t22Z3aUpQWyesUyXJDIH_nbVvX49NGbmJ5OrJAEcDSu5YJb6zanQBdoQUquLNehgmRs7QJnkQAScy_f1mA1VPKSZjoU8YqJRVT6QQ6ynnIwOC8QWRQ1zchGrAvbI1FVpNfnCtaZ8T2Qh9nJoTiO4UAxAhwYAJRTBDJ8fvNPP2MOJJxE_OejHlBVIN0w3kwlpmKrOUFgcvPODUbwLSdx95zuwb9fK9nC60LlMs4Yu-FhQkdNmmi9P816QtM_FI7ZdlAOdCcgrGJzyX1cebXw1icebCL0YpB1gnqO2mqByLLEgUIJ2HCk7K3i2Bo6fE22-nny7byJR3mpBttpWbB7je5VqKxEtRNPDN8b31oPTjALkxMeOXZE6u5YfBM0hWNnZ2aCyTt8cHEXPup2wQ=s100-no",
+                        'vehicleImg': "https://lh3.googleusercontent.com/YmXUsCJPHTC_SBZLVfnlVXNvoq4I-2_x6FN5hPa-l1et1cKW82wzGU7K6L6sAxWrt2J4X97B7Cvk7OKVxSv_-ahAygLqTeqgQqS7rFpAAi50Ab1JFkBcxBoV_Od29Dj9aBhlQQdXuenMYTpvKkbcorZAQeDhbAvU7B5q_21iKfzzaNgenhb-1i9dn3W56EG-r5anawdg4pyzJPHXFmOMZIb0zlxMhSfGxWE2-Ws0VwvNI1so2FJPhgfyQxMFWOeY_zEUc6KrEQkCkxNq-3qDtDNT7YrFaGQH5xCX8t22Z3aUpQWyesUyXJDIH_nbVvX49NGbmJ5OrJAEcDSu5YJb6zanQBdoQUquLNehgmRs7QJnkQAScy_f1mA1VPKSZjoU8YqJRVT6QQ6ynnIwOC8QWRQ1zchGrAvbI1FVpNfnCtaZ8T2Qh9nJoTiO4UAxAhwYAJRTBDJ8fvNPP2MOJJxE_OejHlBVIN0w3kwlpmKrOUFgcvPODUbwLSdx95zuwb9fK9nC60LlMs4Yu-FhQkdNmmi9P816QtM_FI7ZdlAOdCcgrGJzyX1cebXw1icebCL0YpB1gnqO2mqByLLEgUIJ2HCk7K3i2Bo6fE22-nny7byJR3mpBttpWbB7je5VqKxEtRNPDN8b31oPTjALkxMeOXZE6u5YfBM0hWNnZ2aCyTt8cHEXPup2wQ=s100-no",
+                        'lat': bike[1],
+                        'lng': bike[0],
+                        'color': '#3D8999',
+                        'pricePerUnit': "1 € / 30 Min.",
+                        'website': "https://www.fordpass-bike.de/de",
+                        'deeplink': 'nextbike://'
                     })
 
                 });
@@ -48,12 +48,9 @@ export function fetchFordbike() {
 
                 resolve(list);
 
+            })
 
-            }).catch(function() {
-                console.log("SCHEISE");
-              });
 
-              
 
 
     })
