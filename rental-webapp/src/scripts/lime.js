b2c481592e48de36e358772677ed70c8418ed804
Carlos <carlosbystron@yahoo.de>
2019-09-23 21:55:08 +0200

    Lime gefixt

--- rental-webapp/src/scripts/lime.js
+++ rental-webapp/src/scripts/lime.js
@@ -8,9 +8,8 @@ export function fetchLime() {
       var lat = position.coords.latitude
       var lng = position.coords.longitude
 
-      var cityID = "COLOGNE";
       var url = 'https://web-production.lime.bike/api/rider/v1/views/map';
-      var api = "?ne_lat=52&ne_lng=7.4&sw_lat=50&sw_lng=6.2&user_latitude=" + lat + "&user_longitude=" + lng + "&zoom=160";
+      var api = "?ne_lat="+ lat +"&ne_lng="+ lng +"&sw_lat="+ lat +"&sw_lng="+ lng +"&user_latitude=" + lat + "&user_longitude=" + lng + "&zoom=60";
       var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
 
@@ -40,12 +39,12 @@ export function fetchLime() {
                 'battery': true,
                 'batteryLevel': scooter.attributes.battery_level,
                 'icon' : { 
-                url: require('../assets/icons/Lime.png'),
-                size: new google.maps.Size(100, 150),
-                scaledSize: new google.maps.Size(36, 51),
-                origin: new google.maps.Point(0, 0), // origin
-                anchor: new google.maps.Point(18, 45) // anchor
-              },
+                  url: require('../assets/icons/Lime.png'),
+                  size: new google.maps.Size(100, 150),
+                  scaledSize: new google.maps.Size(36, 51),
+                  origin: new google.maps.Point(0, 0), // origin
+                  anchor: new google.maps.Point(18, 45) // anchor
+                },
                 'logo': 'https://lh3.googleusercontent.com/iLn5nb6rai-f0GspwuWhO22yM8ZESKuERCRYD4hzBWoijYhN0VsCa8MImZxMHjgTyVtlUkzFHK0FyaTHnxprZm6c3DJI28MGupulZfZwWrXkgjeA0MndunoTYbDFVHNtB7nQRzdCgIJNN8p8jIiPCbvQQkKHxgWrQXNsOu5tEoOgVOBfV2HXph-cUAI-u5JHnXxn1eDaG2CwwTOgG8GQKGLgfUoKnN0qwA7OPhAvyPdGAOGvfQHIRZGJpiEWg1dgDI9b2TeYKblUrG1ksQBt96dE1BZItXhkbbIwpbisjfb_3Xm_JUyEp6TGu6nBPxYF5M7yhcpfwiKFnQJevHTv_uwL6qdogCuf18qENA8cPGz9e4t_-ECKOl9zzfDUxrN8B9KQv9ef-vxhTNe5V7-wM-V6GQgtsP4w6srPK--wCiTykXfDqGKbjBSTf4Vr54zERxpb8fRYC-De6SAfOc8iKqwhPl1A5R25JtaKqmj9AjQ3i_5TTHpoCy0p3sQvkepOtfpKRINw99j-44OMa4EQTWo3UyB6QHGmnpjV0jFsmveX_mQfXkluofPcLmIxygkmzablhrxUvCxRgc4-EFyuLN6LaO9Jd7VsewwysLJ2ZMzKibgbin6th_EjPsbPQakyIAzkxpy-XCNc98ESIQ_4jwOFbcCTRlH12KS9ZYa7fW3RJTXlH4rVzQ=s452-no',
                 'vehicleImg': 'https://lh3.googleusercontent.com/5d2bIdVfIXw__CZCH7-g9vrhZDaRq_XN9cNu3Wc4xhgKXofP2Joxoo3LTaFSnexZ8n05OHEmTa9t5v9P4w=w328-h315-no',
                 'lat': scooter.attributes.latitude,
