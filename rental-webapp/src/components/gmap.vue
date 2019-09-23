805d3dc7722ce45253de4b36940e5638db6460d5
Carlos <carlosbystron@yahoo.de>
2019-09-23 16:24:11 +0200

    Neues Map Styling

--- rental-webapp/src/components/gmap.vue
+++ rental-webapp/src/components/gmap.vue
@@ -56,88 +56,61 @@
 			gestureHandling : 'greedy',
 			disableDefaultUI : true,
 			styles: [
-						{elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-						{elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-						{elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
 						{
-							featureType: 'administrative.locality',
-							elementType: 'labels.text.fill',
-							stylers: [{color: '#d59563'}]
+              elementType: 'geometry',
+              stylers: [{color: '#011b26'}]
+              },
+						{
+              elementType: 'labels.icon',
+              stylers: [{visibility: 'off'}]
+            },
+            {
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#021a26'},{lightness: -5},{visibility: 'on'}]
+            },
+						{
+              elementType: 'labels.text.stroke',
+              stylers: [{weight: 1.5}]
+            },
+						{
+							featureType: 'administrative',
+							elementType: 'geometry',
+							stylers: [{color: '#757575'}]
 						},
 						{
-							featureType: 'poi',
-							elementType: 'labels.text',
-							stylers: [{visibility: 'off'}]
-						},
-						{
-							featureType: 'poi',
-							elementType: 'labels.icon',
+							featureType: 'administrative.land_parcel',
+							elementType: 'geometry',
 							stylers: [{visibility: 'off'}]
 						},
 						{
 							featureType: 'poi.park',
-							elementType: 'geometry',
-							stylers: [{color: '#315943'}]
+							elementType: 'geometry.fill',
+							stylers: [{color: '#002226'}]
 						},
 						{
 							featureType: 'road',
-							elementType: 'geometry',
-							stylers: [{color: '#265c63'}]
-						},
-						{
-							featureType: 'road',
-							elementType: 'geometry.stroke',
-							stylers: [{color: '#212a37'}]
-						},
-						{
-							featureType: 'road',
-							elementType: 'labels.text.fill',
-							stylers: [{color: '#9ca5b3'}]
-						},
-						{
-							featureType: 'road.highway',
-							elementType: 'geometry',
-							stylers: [{color: '#746855'}]
-						},
-						{
-							featureType: 'road.highway',
-							elementType: 'geometry.stroke',
-							stylers: [{color: '#1f2835'}]
-						},
-						{
-							featureType: 'road.highway',
-							elementType: 'labels.icon',
-							stylers: [{visibility: 'off'}]
-						},
-						{
-							featureType: 'road.highway',
-							elementType: 'labels.text.fill',
-							stylers: [{visibility: 'off'}]
+							elementType: 'geometry.fill',
+							stylers: [{color: '#496773'}]
 						},
 						{
 							featureType: 'transit',
+							elementType: 'geometry.fill',
+							stylers: [{color: '#2f4d59'}]
+						},
+						{
+              featureType: 'transit.station.bus',
+              elementType: 'labels.icon',
+              stylers: [{saturation: -60},{visibility: 'on'}]
+            },
+						{
+              featureType: 'transit.station.rail',
+              elementType: 'labels.icon',
+              stylers: [{saturation: -60},{visibility: 'on'}]
+            },
+						{
+							featureType: 'water',
 							elementType: 'geometry',
-							stylers: [{color: '#2f3948'}]
-						},
-						{
-							featureType: 'transit.station',
-							elementType: 'labels.text.fill',
-							stylers: [{color: '#d59563'}]
-						},
-						{
-							featureType: 'water',
-							elementType: 'geometry',
-							stylers: [{color: '#17263c'}]
-						},
-						{
-							featureType: 'water',
-							elementType: 'labels.text.fill',
-							stylers: [{color: '#515c6d'}]
-						},
-						{
-							featureType: 'water',
-							elementType: 'labels.text.stroke',
-							stylers: [{color: '#17263c'}]
+							stylers: [{color: '#01111e'}]
 						}
 					]
 				}"
